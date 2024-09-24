668dbf0d72833d039a9f53c6003ee203eb3c5c3e
江俊鸿 <1414197421@qq.com>
2024-09-24 18:29:45 +0800

    fix

--- pages/editor/index.vue
+++ pages/editor/index.vue
@@ -1,16 +1,9 @@
 <template>
-  <MdEditor v-model="text">
-    <template #defToolbars>
-          <Mark />
-          <Emoji />
-        </template>
-  </MdEditor>
+
 </template>
 
 <script setup lang="ts">
-import { MdEditor } from 'md-editor-v3';
-import { Emoji, Mark } from '@vavt/v3-extension';
-import 'md-editor-v3/lib/style.css';
+import Vditor from 'vditor'
+import "~vditor/src/assets/less/index"
 
-const text = ref('Hello Editor!');
 </script>
